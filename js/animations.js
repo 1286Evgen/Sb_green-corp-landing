d814b74f3dbd99ef56b918f0cf4c64204c9b0df7
1286Evgen <lopatingenia@rambler.ru>
2023-01-27 00:59:36 -0300

    27en1

--- js/animations.js
+++ js/animations.js
@@ -13,39 +13,40 @@ function increaseNumberAnimationStep (i, element, endNumber) {
    }
 }
 
-
 function initIncreaseNumberAnimation() {
     let element = document.querySelector('.features__clients-count');
     increaseNumberAnimationStep(0, element, 5000);
   }
-initIncreaseNumberAnimation();
+
+  initIncreaseNumberAnimation();
+
 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
   if (event.target.value === 'other') {
     const formContainer = document.createElement('div');
     formContainer.classList.add('form__group');
-    formContainer.classList.add('form__other-input');
+    formContainer.classList.add('form__other-input'); 
  
     const input = document.createElement('input');
     input.placeholder = "Введите ваш вариант";
-    input.type = "text";
- 
+    input.type = "text"; 
+      
     formContainer.appendChild(input);
-    document.querySelector('.form form').insertBefore(formContainer, document.querySelector('.form__submit')); 
+    document.querySelector('.form form').insertBefore(formContainer, document.querySelector('.form__submit'));
   }
  
-  let otherInput = document.querySelector('.form__other-input');
-  if (event.target.value !== 'other' && Boolean(otherInput)) {
-      document.querySelector('.form form').removeChild(otherInput); 
+  const otherInput = document.querySelector('.form__other-input');
+  if (event.target.value !== 'other' && otherInput) { 
+  document.querySelector('.form form').removeChild(otherInput); 
   }
 });
 
 function updateScroll() {
   if(window.scrollY>0) {
-    document.querySelector(header).classList.add('header__scrolled');
+    document.querySelector('header').classList.add('header__scrolled');
   }
   else {
-    document.querySelector(header).classList.remove('header__scrolled');
+    document.querySelector('header').classList.remove('header__scrolled');
   }
-
+}
 window.addEventListener("scroll", updateScroll);
