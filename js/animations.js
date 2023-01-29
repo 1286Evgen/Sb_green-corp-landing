0dedba7a02797efc2b9e496feb654a87a68cfb3f
1286Evgen <lopatingenia@rambler.ru>
2023-01-29 12:48:19 -0300

    29en4

--- js/animations.js
+++ js/animations.js
@@ -47,13 +47,13 @@ function updateScroll() {
   else {
     document.querySelector('header').classList.remove('header__scrolled');
   }
-/*
+
   let windowBottomPosition = window.scrollY + window.innerHeight;
   let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
 
   if((windowBottomPosition >= countElementPosition) && !animationInited) {
     animationInited = true;
     initIncreaseNumberAnimation();
-  }*/
+  }
 }
 window.addEventListener("scroll", updateScroll);
