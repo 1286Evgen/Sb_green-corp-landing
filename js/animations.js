3aa798a78fd23b6e86dfd78258e27fbff048e263
1286Evgen <lopatingenia@rambler.ru>
2023-01-29 12:46:13 -0300

    29en1

--- js/animations.js
+++ js/animations.js
@@ -18,9 +18,6 @@ function initIncreaseNumberAnimation() {
     increaseNumberAnimationStep(0, element, 5000);
   }
 
-  initIncreaseNumberAnimation();
-
-
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
   if (event.target.value === 'other') {
     const formContainer = document.createElement('div');
@@ -41,12 +38,22 @@ document.querySelector('#budget').addEventListener('change', function handleSele
   }
 });
 
+let animationInited = false;
+
 function updateScroll() {
   if(window.scrollY>0) {
     document.querySelector('header').classList.add('header__scrolled');
   }
   else {
     document.querySelector('header').classList.remove('header__scrolled');
+  }
+
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+
+  if((windowBottomPosition >= countElementPosition) && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
   }
 }
 window.addEventListener("scroll", updateScroll);
